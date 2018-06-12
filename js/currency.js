af7e1cdd60e02859cfdeebe9dfd9e94ab7c87ee4
JooYoo <jooyoo@outlook.com>
2018-06-12 09:52:05 +0200

    api works

--- js/currency.js
+++ js/currency.js
@@ -1,28 +1,26 @@
 
-var myReq = new XMLHttpRequest();
-myReq.open('GET','json file path','true');
-myReq.onload=function(){
-    var result = JSON.parse(myReq.responseText);
-    console.log(result);
-}
-myReq.send();
-
 function convertCurrency(){
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
 
     var xmlhttp = new XMLHttpRequest();
-    var url = "http://api.fixer.io/latest?symbols=" + from + "," + to;
+    var accessKey = "0d48bf0a46d9dd953c1b12395ef89565";
+    var url = "http://data.fixer.io/latest"
+                +"?access_key="+accessKey
+                +"&symbols=" + from + "," + to;
     xmlhttp.open('GET', url, true);
     xmlhttp.send();
     
     xmlhttp.onreadystatechange = function(){
         if(xmlhttp.readyState === 4 && xmlhttp.status == 200){
-
+            // get data
             var result = xmlhttp.responseText;
-            alert(result);
+            // parse to json data
             var jsResult = JSON.parse(result);
+            // calculate
+            var oneUnit = jsResult.rates[to] / jsResult.rates[from];
+            var amt = document.getElementById("fromAmount").value;
+            document.getElementById("toAmount").value = (oneUnit * amt).toFixed(2);
         }
     }
-
 } 
